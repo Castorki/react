540d0f5a96c9646d420c184c69f671b2bab7d28c
Darya Kauzova <amayasuzuki12@gmail.com>
2025-04-14 14:36:29 +0300

    fixed homeworks structure and display

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import './App.css';
+import CommentsList from './components/CommentsList';
 import TemperatureConverter from './components/TemperatureConverter';
 import ToDoList from './components/TodoList ';
 
 function App() {
   return (
     <div className='main'>
+
+      {/* Домашние задание к семинару 1 */}
+
+      {/* <header className='header'>
+        <News country="Южная Корея"
+          article="На этой неделе погода в Южной Корее будет переменчивой: от солнечных дней до кратковременных дождей. Температура остаётся комфортной для начала весны, но в некоторых регионах возможны прохладные ночи." />
+      </header> */}
+
+
+      {/* Домашние задание к семинару 2 */}
+
+      {/* <div className='comments'>
+        <h2>Список комментариев</h2>
+        <ul>
+          <CommentsList />
+        </ul>
+      </div> */}
+
+
+      {/* Домашние задание к семинару 3 */}
+
       <div className='degrees__Calculater'>
         <h2>Калькулятор градусов</h2>
         <TemperatureConverter />
@@ -15,6 +37,15 @@ function App() {
       </div>
     </div>
   );
+}
+
+function News(props) {
+  return (
+    <div>
+      <h1 style={{ color: "green" }} className='header__greeting'>Привет, {props.country}!</h1>
+      <article style={{ color: "purple" }} className='header__article'>{props.article}</article>
+    </div>
+  )
 }
 
 export default App;
